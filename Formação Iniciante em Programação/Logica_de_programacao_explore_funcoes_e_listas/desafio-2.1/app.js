4c4531bf61375c5426e70a1f40655e841b328d7d
Amanda Oliveira | Plano Angelus <amanda.oliveira@planoangelus.com.br>
2024-07-12 11:09:44 -0300

    commit pasta inteira

---
+++ Formação Iniciante em Programação/Logica_de_programacao_explore_funcoes_e_listas/desafio-2.1/app.js
@@ -0,0 +1,56 @@
+// function HelloWorld() {
+//     console.log('Olá, Mundo!');
+// }
+
+//Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+// function Saudacao(nome){
+//     console.log(`Olá, ${nome}`);
+// }
+// function SaudacaoHandler(){
+//     let nome = document.querySelector('input').value;
+//     Saudacao(nome)
+// }
+
+//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+// let numero = 5;
+// let resultado = CalcularDobro(numero);
+// console.log(resultado)
+
+// function CalcularDobro(numero){
+//    return numero * 2;    
+// }
+
+//Criar uma função que recebe três números como parâmetros e retorna a média deles.
+// let numero1 = 8;
+// let numero2 = 4;
+// let numero3 = 10;
+// let media = CalcularMedia(numero1, numero2, numero3);
+
+// function CalcularMedia(numero1, numero2, numero3){
+//     return (numero1 + numero2 + numero3)/3;
+// }
+// console.log(media)
+
+//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+// let numero1 = 8;
+// let numero2 = 7;
+// let maiorNumero = NumeroMaior(numero1,numero2);
+
+// function NumeroMaior(numero1, numero2){
+//     if(numero1 > numero2){
+//         return numero1;
+//     }else{
+//         return numero2;
+//     }
+// }
+// console.log(maiorNumero); */
+
+//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+// let numero = 3;
+// let resultado = multiplicacao(numero)
+
+// function multiplicacao(numero){
+//     return numero *= numero;
+// }
+
+// console.log(resultado)
